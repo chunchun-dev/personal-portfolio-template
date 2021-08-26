709d8f5f8943d649b8a99d9836977525f8f4425a
qt-coder <chuchenkhang@gmail.com>
2021-08-26 10:35:57 +0800

    Changed projects to use config file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { About } from "../components/About";
 import { Hero } from "../components/Hero";
 import { Projects } from "../components/Projects";
+import UserContext from "../userConfig";
 
 export default function Home() {
+
+  const {projects} = useContext(UserContext)
+
   return (
     <div>
       <Hero
@@ -13,14 +18,7 @@ export default function Home() {
         '
         cta={{ title: 'CTA', link: '' }}
       />
-      <Projects projects={[
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60' },
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60' },
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60' },
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60' },
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60' },
-        { title: 'Project 1', desc: 'a great project', link: '', img: 'https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60' },
-      ]}/>
+      <Projects projects={projects}/>
       <About desc='Occaecat sunt magna ipsum cillum. Id eiusmod minim id dolore. Mollit sint sit amet amet labore do enim velit do minim dolore fugiat. Esse nisi excepteur sit dolore sint qui laborum incididunt est culpa consequat irure tempor dolor. Laborum quis nisi velit amet aute adipisicing commodo dolor elit aliquip. Elit ea labore id reprehenderit qui. Consectetur eu nisi esse labore tempor deserunt esse tempor pariatur proident est. Esse enim magna excepteur esse incididunt commodo deserunt tempor officia ipsum.'/>
     </div>
   )
