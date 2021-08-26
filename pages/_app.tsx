db87306b5adf090fd0d0d96ec8543f2d9e0fc835
qt-coder <chuchenkhang@gmail.com>
2021-08-26 10:24:59 +0800

    Added page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import { createContext } from 'react'
 import { Navbar } from '../components/Navbar'
 import '../styles/globals.css'
 import UserContext, {UserConfig} from '../userConfig'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserContext.Provider value={UserConfig}>
+      <Head>
+        <title>{UserConfig.name}</title>
+      </Head>
       <Navbar navItems={[ 'navbar item', 'item 2' ]}/>
       <Component {...pageProps} />
     </UserContext.Provider>
